c747b2dee9977d700be0da50e72e366cacde3590
DivyaPai89 <dhp.hld@gmail.com>
2021-09-25 14:56:02 +0530

    version 1.0

---
+++ DP1.js
@@ -0,0 +1,27 @@
+let Number1 = 10,Number2 = 20;
+
+//Addition
+let Number3 = Number1 + Number2;
+console.log(Number3);
+
+//Substraction
+Number3 = Number1-Number2;
+console.log(Number3);
+
+//Multiplication
+Number3 = Number1*Number2;
+console.log(Number3);
+
+//Division
+Number3 = Number1/Number2;
+console.log(Number3);
+
+//Percentage
+Number3 = Number1 % Number2;
+console.log(Number3);
+
+let FirstName='Divya', LastName='Pai';
+
+//Concatenation
+let Name = FirstName + LastName;
+console.log(Name);
